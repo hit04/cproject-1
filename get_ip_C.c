2068861fb6243bd7f9a02e03193f7367612f4318
hit04 <singhhitendra04@gmail.com>
2019-06-17 13:25:53 +0530

    Add files via upload

---
+++ get_ip_C.c
@@ -0,0 +1,15 @@
+
+#include<stdlib.h>
+
+int main()
+{
+   //system("C:\\Windows\\System32\\ipconfig");
+
+   return 0;
+}
+/* program to get ip address
+This c program prints ip (internet protocol) address of your computer, system function is used to execute the command ipconfig which prints ip address, subnet mask and default gateway. The code given below works for Windows xp and Windows 7. If you are using turbo c compiler then execute program from folder, it may not work when you are working in compiler and press Ctrl+F9 to run your program.
+C programming code
+
+
+*/
